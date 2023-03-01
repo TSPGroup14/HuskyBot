143fd513d66fa79425d3cd00acb057d1da344dec
PhantomStray <lucasbohanon@gmail.com>
2023-03-01 11:36:16 -0500

    Update: - Add basic ban command

---
+++ src/main/java/huskybot/commands/moderation/Ban.kt
@@ -0,0 +1,10 @@
+package huskybot.commands.moderation
+
+import huskybot.cmdFramework.*
+
+@CommandProperties(description = "Ban a requested user")
+class Ban : Command(ExecutionType.STANDARD) {
+    override fun execute(context: Context) {
+
+    }
+}
