2a3850e6d80d32e5253c64a4871f001553fffbc8
PhantomStray <lucasbohanon@gmail.com>
2023-03-01 12:02:42 -0500

    Update: - Add required and non-required options to ban command

--- src/main/java/huskybot/commands/moderation/Ban.kt
+++ src/main/java/huskybot/commands/moderation/Ban.kt
@@ -1,8 +1,15 @@
 package huskybot.commands.moderation
 
 import huskybot.cmdFramework.*
+import net.dv8tion.jda.api.interactions.commands.OptionType
 
 @CommandProperties(description = "Ban a requested user")
+@Options([
+        Option(name = "user", description = "User that you would like to ban", type = OptionType.USER, required = true),
+        Option(name = "reason", description = "Reason for banning the user", type = OptionType.STRING, required = false),
+        Option(name = "duration", description = "How long the ban will last for", type = OptionType.INTEGER, required = false),
+        Option(name = "days", description = "How many days worth of messages from the user to remove", OptionType.INTEGER, required = false)
+    ])
 class Ban : Command(ExecutionType.STANDARD) {
     override fun execute(context: Context) {
 
