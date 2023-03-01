027a5628c5b8209b5a83020e6f9c7c15dee7a6c4
PhantomStray <lucasbohanon@gmail.com>
2023-03-01 11:44:50 -0500

    Update: - Fix ban command

--- src/main/java/huskybot/commands/moderation/Ban.kt
+++ src/main/java/huskybot/commands/moderation/Ban.kt
@@ -3,7 +3,6 @@ package huskybot.commands.moderation
 import huskybot.cmdFramework.*
 
 @CommandProperties(description = "Ban a requested user")
-@Options()
 class Ban : Command(ExecutionType.STANDARD) {
     override fun execute(context: Context) {
 
