3c270885ba4b92e7f6751a9eb1edf0fefd6e37a3
Malakai Bundshuh <bundshuhm@gmail.com>
2023-04-14 12:04:50 -0400

    Level Manager logic, Level command should display lvl and update it before.

--- src/main/java/huskybot/commands/misc/Level.kt
+++ src/main/java/huskybot/commands/misc/Level.kt
@@ -10,11 +10,13 @@ import net.dv8tion.jda.api.entities.MessageEmbed
 class Level : Command(ExecutionType.STANDARD) {
     override fun execute(ctx: Context) {
         val xp = ctx.guild?.let { Database.getUserXP(it.idLong, ctx.member.user.idLong) }
-
+        var lvl = ctx.guild?.let { Database.getUserLevel(it.idLong, ctx.member.user.idLong) }
+        ctx.guild?.let { Database.updateUserLevel(it.idLong, ctx.member.user.idLong, lvl!!) }
+        lvl = ctx.guild?.let { Database.getUserLevel(it.idLong, ctx.member.user.idLong) }
         ctx.embed{
             setTitle("Your Level")
             addField(MessageEmbed.Field("XP:", "$xp", true))
-            addField(MessageEmbed.Field("Level:", "(TBA)", true))
+            addField(MessageEmbed.Field("Level:", "$lvl", true))
             setThumbnail(ctx.member.user.avatarUrl)
         }
     }
