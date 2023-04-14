4f71376c6ed6e286f66dbb76829f91d61056a4af
Malakai Bundshuh <bundshuhm@gmail.com>
2023-04-14 11:52:08 -0400

    Level Manager logic, Level command should display lvl and update it before.

--- src/main/java/huskybot/modules/leveling/LevelManager.kt
+++ src/main/java/huskybot/modules/leveling/LevelManager.kt
@@ -1,6 +1,21 @@
 package huskybot.modules.leveling
 
+import huskybot.cmdFramework.Context
+import kotlin.math.*
+
 object LevelManager {
-
-
+    private fun calcLevel (xp : Int, lvl : Int): Int {
+        val r = 1.9
+        var highest = 0
+        var x = 100.0
+        do {
+            x *= r
+            highest++
+        } while (highest < lvl+1)
+        return if (xp >= x) {
+            lvl+1
+        } else {
+            lvl
+        }
+    }
 }
